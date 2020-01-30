e7fb20847e3170783c39f1a4161763e7fb8548f0
Jordon <Jordonrunge20@gmail.com>
2020-01-30 02:25:51 -0600

    Ive done most of the javascript and styling. I just need to finsh the saving of information

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,8 +12,8 @@ var times = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 var now = moment().format()
 console.log(now)
-var TheHour = moment().hour()
-console.log(TheHour)
+var theHour = moment(11, "hh").hour()
+console.log(theHour)
 
 
 
@@ -31,6 +31,7 @@ for(var i = 0; i < times.length; i++){
         timeBlockColTime.append(cellTextTime)
     // Table Col Input, Creating and appending, has an ID hour-times[i], has data-input - times[i]
     var timeBlockColInput = document.createElement("td");
+        // timeBlockColInput.setAttribute("class", "b")
         timeBlockColInput.setAttribute("id", "input-" + times[i])
         timeBlockColInput.setAttribute("data-input", times[i]);
     var cellTextInput = document.createTextNode("", createInput());
@@ -69,6 +70,8 @@ timeBlockContainerTableBody.addEventListener("click", function(event){
         console.log(inputBlockText)
         // console.log(buttonValue)
 
+
+
         // Doesnt Let you enter Nothing
         if(inputBlockText === null || inputBlockText === ""){
     
@@ -77,6 +80,7 @@ timeBlockContainerTableBody.addEventListener("click", function(event){
             if(buttonValue === "unlocked"){
                 colButton.setAttribute("data-value" ,"locked");     
                 chaingingBackGround();
+                saveData();
             // Turns Locked Button to Unlocked 
             } else if(buttonValue === "locked"){
                 colButton.setAttribute("data-value" ,"unlocked");     
@@ -91,6 +95,7 @@ timeBlockContainerTableBody.addEventListener("click", function(event){
 // Creates the Input Box in the Table
 function createInput(){
     var timeInput = document.createElement("input");
+    timeInput.setAttribute("class", "b input-group input-group-lg");
     timeInput.setAttribute("type", "text");
     timeBlockColInput.append(timeInput);
 }
@@ -99,42 +104,57 @@ function createButton(){
     var timeButton = document.createElement("button")
     timeButton.setAttribute("class", 'b btn btn-success btn-block')
     timeButton.setAttribute("data-value", 'unlocked')
-    timeButton.setAttribute("type", 'submit')
+    timeButton.setAttribute("type", 'button')
     timeBlockColButton.append(timeButton)
 }
-
+// Follows the time and Changes the background grey(Past Time), green(Unlocked), red(Locked)
 function chaingingBackGround(){
 
-    if(TheHour >= 8 || TheHour <= 17){
-        
-        // Starts at hour 8
-        for(i = 8; i<= times.length + 7; i++){
+        // Tracks the Working time Block
+        for(i = 8; i <= times.length + 7; i++){
             var checkInputId = document.querySelector("#input-"+i)
             var checkButtonId = document.querySelector("#button-"+i).children[0]
             var checkButtonValue = checkButtonId.getAttribute("data-value")
         // If theHour is out the bounds of i, background is Grey, Set the grey Button class 
-        if(i < TheHour){
+        if(theHour > i){
             checkInputId.style.background = "grey";
             checkButtonId.setAttribute("class", "b btn btn-secondary btn-block .disabled");
             checkButtonId.textContent = "Maybe Tomorrow"
-         // Else Locked or Unlocked
-        } else{
-        // If checkButtonValue is locked, background is red, Set the button red, locked symbol
-        if(checkButtonValue === "locked"){
-            checkButtonId.setAttribute("class", "b btn btn-danger btn-block fas fa-lock")
-            checkInputId.style.background = "red";
+         
         }
-        // If checkButtonValue is unlocked, background is green, Set the button green, unlocked symbol
-        if(checkButtonValue === "unlocked"){
-            checkButtonId.setAttribute("class", "b btn btn-success btn-block fas fa-unlock")
-            checkInputId.style.background = "green";
-        }  
+        // Else Locked or Unlocked
+        else{
+
+            // If checkButtonValue is locked, background is red, Set the button red, locked symbol
+            if(checkButtonValue === "locked"){
+                checkButtonId.setAttribute("class", "b btn btn-danger btn-block fas fa-lock");
+                checkInputId.style.background = "red";
+                // saveData();
             }
+            // If checkButtonValue is unlocked, background is green, Set the button green, unlocked symbol
+            if(checkButtonValue === "unlocked"){
+                checkButtonId.setAttribute("class", "b btn btn-success btn-block fas fa-unlock");
+                checkInputId.style.background = "green";
+                // deleteData();
+            }         
         }
-    } 
+    }   
 }
 
 function saveData(){
+    for(i = 8; i <= times.length + 7; i++){
+        var checkInputId = document.querySelector("#input-"+i)
+        
+        var inputValue = checkInputId.children[0].value
+console.log(checkInputId)
+        var savedInputData = [];
+            savedInputData.push(inputValue)
+    }
+    
+   console.log(savedInputData)
+
+    
+
 
 }
 
